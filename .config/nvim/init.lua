13f71d6ad5d600eaa3816f98f4219a6720df52c5
Pranit <82385936+Pranit986@users.noreply.github.com>
2024-11-07 19:48:34 +0530

    Add files via upload

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.opt.background = "dark" -- set this to dark or light
+vim.cmd("colorscheme oxocarbon")
+
+vim.opt.guicursor = ""
